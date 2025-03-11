32ba4ab82455ff93a91dfede3764f566b96d86b2
Maksym Mishchenko <makcim200111@gmail.com>
2025-03-11 17:57:01 +0200

    оновив, так як не працювало з sqlite

--- config/config.ts
+++ config/config.ts
@@ -4,15 +4,27 @@ dotenv.config();
 
 export const config = {
   development: {
-    dialect: process.env.DB_DIALECT,
-    storage: process.env.DB_STORAGE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres"
   },
   test: {
-    dialect: process.env.DB_DIALECT,
-    storage: process.env.DB_STORAGE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres"
   },
   production: {
-    dialect: process.env.DB_DIALECT,
-    storage: process.env.DB_STORAGE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres"
   },
 };
